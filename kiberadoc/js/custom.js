87f535dc0f1f8f70343771b5a8af6f1a6e66c1da
I-Create-Portfolio <gordwincreative@gmail.com>
2025-03-14 15:44:33 +0300

    Add files via upload

---
+++ kiberadoc/js/custom.js
@@ -0,0 +1,119 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+
+// owl carousel slider js
+var owl = $('.project_carousel').owlCarousel({
+    loop: false,
+    margin: 15,
+    center: true,
+    startPosition: 2,
+    autoplay: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            center: false,
+            items: 1,
+            margin: 0
+        },
+        769: {
+            items: 2,
+        },
+        992: {
+            center: true,
+            items: 3
+        }
+    }
+})
+
+
+// owl.owlcarousel2_filter
+
+$('.owl-filter-bar').on('click', '.item', function (e) {
+    var $items = $('.owl-filter-bar a')
+    var $item = $(this);
+    var filter = $item.data('owl-filter')
+    $items.removeClass("active");
+    $item.addClass("active");
+    owl.owlcarousel2_filter(filter);
+
+    e.preventDefault();
+})
+
+
+$(document).ready(function (){
+
+	$("#email_submit").click(function (){
+
+		var $email_data_var;
+		$email_data_var = $("#email_data").val();
+		if($email_data_var == ''){
+			$("#email_msg").html("Please Enter a Email Address");
+		}
+		else{
+
+			$.ajax({
+
+				type:'POST',
+				url:"ajax/email-submit.php",
+				data:{email_data_values : $email_data_var},
+				success:function(response){
+					$("#email_msg").html(response);
+				}
+
+			});
+
+		}
+
+	});
+
+});
+
+
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+const executeCodes = () => {
+  //if cookie contains codinglab it will be returned and below of this code will not run
+  if (document.cookie.includes("codinglab")) return;
+  cookieBox.classList.add("show");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+      //if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= codinglab; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
+
+
+
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
+
+
+
+
