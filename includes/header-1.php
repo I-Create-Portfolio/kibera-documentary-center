eebbec2922538b0037a8d6435413f74a9a1815b9
I-Create-Portfolio <gordwincreative@gmail.com>
2025-03-14 14:20:43 +0300

    Add files via upload

---
+++ includes/header-1.php
@@ -0,0 +1,27 @@
+ <header class="header_section">
+      <div class="header_top">
+        <div class="container-fluid header_top_container">
+          <div class="lang_box dropdown">
+           <a class="navbar-brand" href="index.php">
+              <h4>KIBERA </BR> DOCUMENTARY<BR> CENTER</h4>
+              <p>Exhibition Area <a href = "../index.php">Home</a></p>
+            </a>
+          </div>
+        
+        </div>
+      </div>
+      <div class="header_bottom">
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-lg custom_nav-container ">
+          
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class=""> </span>
+            </button>
+
+           
+          </nav>
+        </div>
+      </div>
+    </header>
